baceaf8024d9250ec5e66bb4d8c66cc594dc0c64
shiva ghassemzadeh <ghassemzadeh.sh@gmail.com>
2021-05-01 10:07:41 +0430

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import Accordion from './udemy/hook-udemy/Accordion'
 import Search from './udemy/hook-udemy/Search';
 import DropDown from './udemy/hook-udemy/DropDown'
 import { useState } from 'react';
 import Translate from './udemy/hook-udemy/Translate'
+import Header from './udemy/hook-udemy/Header'
+
 const App = ()=>{
 
 
@@ -40,13 +43,24 @@ const [selected,setSelected] = useState(options[0])
 const [showDropDown,setShowDropdown] = useState(true)
 
   return(
+ 
   <div className="ui container">
+     
+    <BrowserRouter>
+    <Header/>
+    <Switch>
+      <Route path="/" exact>  <Accordion items={items}/></Route>
+      <Route path="/search" exact>  <Search/></Route>
+      <Route path="/translate" exact><Translate/></Route>
+      <Route path="/dropdown" exact><DropDown options={options} selected={selected} onSelectedChange={setSelected}/></Route>
+    </Switch>
+ 
 
-  {/* <Accordion items={items}/> */}
-  {/* <Search/> */}
+
   {/* <button onClick ={()=>setShowDropdown(!showDropDown)}>show toggle drop down</button>
   {showDropDown ? <DropDown options={options} selected={selected} onSelectedChange={setSelected}/> :null} */}
-  <Translate/>
+
+  </BrowserRouter>
   </div>
   )
 }
